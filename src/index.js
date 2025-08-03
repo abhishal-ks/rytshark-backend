c1caf6e9b83eac5dd60461ba74ea6c8ad837af49
abhishal-ks <abhishalsharma2002@gmail.com>
2025-08-03 21:44:39 +0530

    fix all register controller bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env' // kaam na kre toh env se phle dot hta lena
 })
 
 
