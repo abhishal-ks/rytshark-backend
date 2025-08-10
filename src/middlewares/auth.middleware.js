5c168bf656cf6e3f096aa66ac4e3c697376046d2
abhishal-ks <abhishalsharma2002@gmail.com>
2025-08-11 00:20:51 +0530

    Add refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -15,7 +15,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => { // req ke baad r
         const user = await User.findById(decodedToken._id).select("-password -refreshToken")
 
         if (!user) {
-            // TSUGI_NO_JUGYU: Discuss about frontend
+
             throw new ApiError(401, "Invalid Access Token")
         }
 
